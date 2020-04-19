115ac3edcb3fb367ffef406deba89977b15c6483
s079208 <s079208@ap.be>
2020-04-19 16:45:47 +0200

    port settings

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ app.use(function(request, response){
 });
 
 
-app.set('port', ());
+app.set('port', (process.env.PORT || 5000));
 // app luisteren naar applicatiepoort
 app.listen( app.get( 'port' ), function() {
-  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  console.log( 'Node server is running on port ' + app.get('port'));
   });
